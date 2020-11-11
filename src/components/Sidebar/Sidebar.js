3eaf1b5dbf37d3dcdae437253953936ebf810823
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-11 22:25:13 +0200

    first

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Sidebar.scss';
+import SidebarMembers from './SidebarMembers/SidebarMembers';
+
+const Sidebar = () => (
+  <>
+    <SidebarMembers />
+  </>
+);
+
+export default Sidebar;
---
+++ src/components/Sidebar/SidebarMembers/SidebarMembers.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SidebarMembers.scss';
+
+const SidebarMembers = () => (
+  <>
+    <ul>
+      <li>
+        Home
+      </li>
+      <li>
+        Contacts
+      </li>
+      <li>
+        About
+      </li>
+    </ul>
+  </>
+);
+
+export default SidebarMembers;
