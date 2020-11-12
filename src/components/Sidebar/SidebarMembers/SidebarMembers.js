2bab6b58555041182e376ae97e13505903d5bd3b
OnlyRaydek <bayramsaid00@gmail.com>
2020-11-13 00:18:15 +0200

    almost done sidebar

--- src/components/Sidebar/SidebarMembers/SidebarMembers.js
+++ src/components/Sidebar/SidebarMembers/SidebarMembers.js
@@ -3,14 +3,20 @@ import './SidebarMembers.scss';
 import membersList from './persons';
 
 const SidebarMembers = () => (
-  <div className="sidebar__members">
-    {membersList.map(img => (
-      <img
-        src={img}
-        alt=""
-        className="sidebar__member"
-      />
-    ))}
+  <div className="sidebar__main">
+    <div className="sidebar__members">
+      {membersList.map(img => (
+        <img
+          src={img}
+          alt=""
+          className="sidebar__member"
+        />
+      ))}
+    </div>
+    <div className="sidebar__settings">
+      <div className="sidebar__grid" />
+      <div className="sidebar__setting" />
+    </div>
   </div>
 );
 
